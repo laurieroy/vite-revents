17cab2e1d7746a5043286a635a2fe86e66caceb9
Laurie Roy <laurieroy.dev@gmail.com>
2024-05-09 15:17:09 -0500

    Add example scratch buttons with slice

--- src/features/scratch/testSlice.ts
+++ src/features/scratch/testSlice.ts
@@ -11,5 +11,17 @@ const initialState: State = {
 export const testSlice = createSlice({
   name: "test",
   initialState,
-  reducers: {}
+  reducers: {
+    increment: (state) => {
+      state.data += 1;
+    },
+    decrement: (state) => {
+      state.data -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.data += action.payload;
+    },
+  },
 });
+
+export const { increment, decrement, incrementByAmount } = testSlice.actions;
